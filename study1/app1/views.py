56b0987d45b85bae52687b02f8d3fa5ad992a64c
myway00 <junior0614@naver.com>
2021-07-10 23:23:27 +0900

    initial

--- study1/app1/views.py
+++ study1/app1/views.py
@@ -2,6 +2,19 @@ from django.shortcuts import render
 def home(request):
     return render(request, "home.html")
 
+def about(request):
+    return render(request, "about.html")
+
 def result(request):
-    userName =  request.GET['name']
-    return render(request, "result.html",{"userName" : userName}) #키 : 값
+    text=request.GET['fulltext'] #home에서 fulltext라는 name의 친구도 가져온다
+    words=text.split() #위에서 받아온 fulltext(text)를 쪼개준다
+    
+    word_dic={} #이 친구를 추가 처음엔 빈 리스트
+    for x in words:
+        if x in word_dic :
+            word_dic[x]+=1
+        else :
+            word_dic[x]=1
+    
+    return render(request, "result.html",{ "total" : text, 'dic':word_dic.items()})
+        #키(result에 가져다 줄 최종 별명) : 값(위의 GET에서 받아온 별명)
--- study1/study1/urls.py
+++ study1/study1/urls.py
@@ -7,4 +7,5 @@ urlpatterns = [
     path('admin/', admin.site.urls),
     path('',views.home, name="home"), #name을 씀으로써 url을 저 name으로 대체가능
     path('result/', views.result, name="result"),
+    path('about/', views.about, name="about"),
 ]
